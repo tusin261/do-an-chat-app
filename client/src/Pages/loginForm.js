1fade11d7415b397912ba012e51f6d727d3231f0
duchunghp444 <hunghuynh2k@gmail.com>
2022-03-11 18:54:26 +0700

    Update Ui

--- client/src/Pages/loginForm.js
+++ client/src/Pages/loginForm.js
@@ -36,31 +36,57 @@ const LoginForm = () => {
   }
 
   return (
-    <Container>
-      <h1>Login</h1>
-      <Row>
-        <Col lg={8}>
-          <Form>
-            <Form.Group className="mb-3" >
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />              
-            </Form.Group>
-            <Form.Group className="mb-3" >
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-            </Form.Group>
+    // <Container>
+    //   <h1>Login</h1>
+    //   <Row>
+    //     <Col lg={8}>
+    //       <Form>
+    //         <Form.Group className="mb-3" >
+    //           <Form.Label>Email address</Form.Label>
+    //           <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />              
+    //         </Form.Group>
+    //         <Form.Group className="mb-3" >
+    //           <Form.Label>Password</Form.Label>
+    //           <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+    //         </Form.Group>
             
-            <Button variant="primary" onClick={handleClick}>
-              Login
-            </Button>
-            <Button >
-              <Link to="/register" className="text-danger">Register</Link>
-            </Button>
-        </Form>
-        </Col>
-      </Row>
-    </Container>
-    
+    //         <Button variant="primary" onClick={handleClick}>
+    //           Login
+    //         </Button>
+    //         <Button >
+    //           <Link to="/register" className="text-danger">Register</Link>
+    //         </Button>
+    //     </Form>
+    //     </Col>
+    //   </Row>
+    // </Container>
+     <div className="container py-5 h-100">
+     <div className="row d-flex justify-content-center align-items-center h-100">
+       <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+         <div className="card bg-dark text-white" style={{ borderRadius: '1rem' }}>
+           <div className="card-body p-5 text-center">
+             <div className="mb-md-5 mt-md-4 pb-5">
+               <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+               <p className="text-white-50 mb-5">Please enter your login and password!</p>
+               <div className="form-outline form-white mb-4">
+                 <input type="email" placeholder='Email' className="form-control form-control-lg" onChange={(e) => setEmail(e.target.value)} />
+                
+               </div>
+               <div className="form-outline form-white mb-4">
+                 <input type="password" placeholder='password' className="form-control form-control-lg" onChange={(e) => setPassword(e.target.value)} />
+                
+               </div>
+               <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
+               <button variant="primary" onClick={handleClick} className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+             </div>
+             <div>
+               <p className="mb-0">Don't have an account? <a href="#!" className="text-white-50 fw-bold">Sign Up</a></p>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
   )
 }
 
--- client/src/Pages/signUpForm.js
+++ client/src/Pages/signUpForm.js
@@ -36,41 +36,79 @@ const SignUpForm = () => {
   }
 
     return (
-      <Container>
-        <h1>Register</h1>
-        <Row>
-          <Col lg={8}>
-            <Form>
-              <Form.Group className="mb-3">
-                <Form.Label>Ho</Form.Label>
-                <Form.Control type="text" placeholder="Enter Ho" onChange={(e) => setLastName(e.target.value)} />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Ten</Form.Label>
-                <Form.Control type="text" placeholder="Enter ten" onChange={(e) => setFirstName(e.target.value)} />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Password lan 2</Form.Label>
-                <Form.Control type="password" placeholder="Password 2" onChange={(e) => setConfirmpassword(e.target.value)} />
-              </Form.Group>
-              <Button variant="primary" onClick={handleClick} >
-                Register
-              </Button>
-              <Button >
-                <Link to="/" className="text-danger">Login</Link>
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
+      // <Container>
+      //   <h1>Register</h1>
+      //   <Row>
+      //     <Col lg={8}>
+      //       <Form>
+      //         <Form.Group className="mb-3">
+      //           <Form.Label>Ho</Form.Label>
+      //           <Form.Control type="text" placeholder="Enter Ho" onChange={(e) => setLastName(e.target.value)} />
+      //         </Form.Group>
+      //         <Form.Group className="mb-3">
+      //           <Form.Label>Ten</Form.Label>
+      //           <Form.Control type="text" placeholder="Enter ten" onChange={(e) => setFirstName(e.target.value)} />
+      //         </Form.Group>
+      //         <Form.Group className="mb-3">
+      //           <Form.Label>Email address</Form.Label>
+      //           <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
+      //         </Form.Group>
+      //         <Form.Group className="mb-3">
+      //           <Form.Label>Password</Form.Label>
+      //           <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      //         </Form.Group>
+      //         <Form.Group className="mb-3">
+      //           <Form.Label>Password lan 2</Form.Label>
+      //           <Form.Control type="password" placeholder="Password 2" onChange={(e) => setConfirmpassword(e.target.value)} />
+      //         </Form.Group>
+      //         <Button variant="primary" onClick={handleClick} >
+      //           Register
+      //         </Button>
+      //         <Button >
+      //           <Link to="/" className="text-danger">Login</Link>
+      //         </Button>
+      //       </Form>
+      //     </Col>
+      //   </Row>
+      // </Container>
+      <div className="container py-5 h-100">
+     <div className="row d-flex justify-content-center align-items-center h-100">
+       <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+         <div className="card bg-dark text-white" style={{ borderRadius: '1rem' }}>
+           <div className="card-body p-5 text-center">
+             <div className="mb-md-5 mt-md-4 pb-5">
+               <h2 className="fw-bold mb-2 text-uppercase">Sign Up</h2>
+               <p className="text-white-50 mb-5">Please enter your information!</p>
+               <div className="form-outline form-white mb-4">
+                 <input type="text" placeholder='Họ' className="form-control form-control-lg" />
+                
+               </div>
+               <div className="form-outline form-white mb-4">
+                 <input type="text" placeholder='Tên' className="form-control form-control-lg" />
+                
+               </div>
+               <div className="form-outline form-white mb-4">
+                 <input type="tel" placeholder='Số điện thoại' className="form-control form-control-lg"  />
+                
+               </div>
+               <div className="form-outline form-white mb-4">
+                 <input type="email" placeholder='Email' className="form-control form-control-lg"  />
+                
+               </div>
+               <div className="form-outline form-white mb-4">
+                 <input type="password" placeholder='Password' className="form-control form-control-lg"  />
+                
+               </div>
+               <button className="btn btn-outline-light btn-lg px-5" type="submit">Sign up</button>
+             </div>
+             <div>
+               <p className="mb-0">Have an account? <a href="#!" className="text-white-50 fw-bold">Sign in</a></p>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
     );
   
 }
